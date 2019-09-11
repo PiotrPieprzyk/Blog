9258ff946a6e39b5ff64d27ab4ece04b3c7b3161
ppieprzyk <piotr.pieprzyk@nxo.pl>
2019-09-11 11:33:22 +0200

    Dodana podstrona rejestracyjna

---
+++ resources/js/components/Auth/RegisterPage.vue
@@ -0,0 +1,3 @@
+<template>
+	<div class="registerWeb">LOL</div>
+</template>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,7 +1,10 @@
 import StartPage from './components/StartPage/StartPage.vue'
 import Galery from './components/Galery/GaleryPage.vue'
+import Register from './components/Auth/RegisterPage.vue'
 
 export const routes = [
   { path: '', component: StartPage },
-  { path: '/galery', component: Galery }
+  { path: '/galery', component: Galery },
+  { path: '/register', component: Register },
+
 ];
